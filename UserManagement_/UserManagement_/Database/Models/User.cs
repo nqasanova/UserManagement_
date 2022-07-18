4ad0bfb5842d53e9e12c8c74a4adbd1dae47c8ff
nqasanova <106176658+nqasanova@users.noreply.github.com>
2022-07-18 10:12:20 +0400

    Update User.cs

--- UserManagement_/UserManagement_/Database/Models/User.cs
+++ UserManagement_/UserManagement_/Database/Models/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,24 +8,28 @@ namespace UserManagement.Database.Models
 {
     internal class User
     {
+        private static int IDCounter;
+        public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public DateTime _registerTime { get; set; }
+        public DateTime RegisterationTime { get; set; }
 
         public User(string firstName, string lastName, string email, string password)
         {
+            ID = IDCounter;
             FirstName = firstName;
             LastName = lastName;
             Email = email;
             Password = password;
-            _registerTime = DateTime.Now;
+            IDCounter++;
+            RegisterationTime = DateTime.Now;
         }
 
         public string GetUserInfo()
         {
-            return $"Name : {FirstName}, Last name : {LastName}, Email : {Email}";
+            return $" ID: {ID}, Name : {FirstName}, Last name : {LastName}, Email : {Email}, Password : {Password}, Registration Time : {RegisterationTime}";
         }
     }
 }
